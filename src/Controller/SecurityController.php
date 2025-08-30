b9f5bff22465803853884006cd5548f0088943bb
dihia-003 <dihia.lr@gmail.com>
2025-08-30 16:49:42 +0200

    Fix CSRF token issues - add missing security configuration and authenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -21,5 +21,12 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Cette méthode peut être vide - elle sera interceptée par la configuration de sécurité
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 } 
